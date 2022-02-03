7e3c954e2b22aafc8834a6d972325bfd419b5e96
Logan Palmer <loganpalmer95@icloud.com>
2022-02-03 18:00:28 -0500

    added fetchAPI for weather and displayed respone

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,77 @@ var formSubmitHandler = function (event) {
         alert("Please enter a City name");
     }
 }
+
+var getWeatherInfo = function (cityname) {
+    var apiCityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&appid=f97301447cbd41068af8623a398ba1fb";
+    fetch(
+        apiCityUrl
+    )
+        .then(function (cityResponse) {
+            return cityResponse.json();
+        })
+        .then(function (cityResponse) {
+            var latitude = cityResponse.coord.lat;
+            var longitude = cityResponse.coord.lon;
+
+            var city = cityResponse.name;
+            var date = (today.getMonth() + 1) + '/' + today.getDate() + '/' + today.getFullYear();
+            var weatherIcon = cityResponse.weather[0].icon;
+            var weatherDescription = cityResponse.weather[0].description;
+            var weatherIconLink = "<img src='http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png' alt='" + weatherDescription + "' title='" + weatherDescription + "'  />"
+
+            currentWeatherEl.textContent = "";
+            fiveDayEl.textContent = "";
+
+            weatherStatusEl.innerHTML = city + " (" + date + ") " + weatherIconLink;
+
+            currentWeatherCardEl.classList.remove("hidden");
+            fiveDayCardEl.classList.remove("hidden");
+
+            return fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&exclude=alerts,minutely,hourly&units=imperial&appid=f97301447cbd41068af8623a398ba1fb');
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            console.log(response)
+            displayWeather(response);
+
+        });
+};
+
+var displayWeather = function (weather) {
+    if (weather.length === 0) {
+        weatherContainerEl.textContent = "No weather data found.";
+        return;
+    }
+    var temperature = document.createElement('p');
+    temperature.id = "temperature";
+    temperature.innerHTML = "<strong>Temperature:</strong> " + weather.current.temp.toFixed(1) + "°F";
+    currentWeatherEl.appendChild(temperature);
+
+    var humidity = document.createElement('p');
+    humidity.id = "humidity";
+    humidity.innerHTML = "<strong>Humidity:</strong> " + weather.current.humidity + "%";
+    currentWeatherEl.appendChild(humidity);
+
+    var windSpeed = document.createElement('p');
+    windSpeed.id = "wind-speed";
+    windSpeed.innerHTML = "<strong>Wind Speed:</strong> " + weather.current.wind_speed.toFixed(1) + " MPH";
+    currentWeatherEl.appendChild(windSpeed);
+
+    var uvIndex = document.createElement('p');
+    var uvIndexValue = weather.current.uvi.toFixed(1);
+    uvIndex.id = "uv-index";
+    if (uvIndexValue >= 0) {
+        uvIndex.className = "uv-index-green"
+    }
+    if (uvIndexValue >= 3) {
+        uvIndex.className = "uv-index-yellow"
+    }
+    if (uvIndexValue >= 8) {
+        uvIndex.className = "uv-index-red"
+    }
+    uvIndex.innerHTML = "<strong>UV Index:</strong> <span>" + uvIndexValue + "</span>";
+    currentWeatherEl.appendChild(uvIndex);
+}
